b313365b2a93a9aac7df9dce74ee574ef4916a42
Joaquin <joaquinlucascortez@gmail.com>
2025-04-03 09:13:38 -0300

    [Feature #005]: Implement correct use of sidebar layout.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="flex flex-col items-center justify-center">
       <h1>Hello Dashboard.</h1>
     </div>
   );
