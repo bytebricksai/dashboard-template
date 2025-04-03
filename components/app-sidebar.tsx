0d199c052b8b2b216d79fc41520dfbb8ea990557
Joaquin <joaquinlucascortez@gmail.com>
2025-04-02 21:17:36 -0300

    [Feature #001]: Restructured sidebar content and layout.

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import * as React from "react";
+import {
+  IconBlocks,
+	IconChartBar,
+	IconDashboard,
+	IconDatabase,
+	IconFolder,
+	IconHelp,
+	IconInnerShadowTop,
+	IconReport,
+	IconSettings,
+} from "@tabler/icons-react";
+
+import { NavMain } from "@/components/nav-main";
+import { NavSecondary } from "@/components/nav-secondary";
+import { NavUser } from "@/components/nav-user";
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+const data = {
+	user: {
+		name: "Usuario Demo",
+		email: "demo@ejemplo.com",
+		avatar: "/avatars/shadcn.jpg",
+	},
+	navMain: [
+		{
+			title: "Principal",
+			url: "/dashboard",
+			icon: IconDashboard,
+		},
+		{
+			title: "Productos",
+			url: "/dashboard/productos",
+			icon: IconFolder,
+		},
+		{
+			title: "Ventas",
+			url: "/dashboard/ventas",
+			icon: IconChartBar,
+		},
+		{
+			title: "Inventario",
+			url: "/dashboard/inventario",
+			icon: IconDatabase,
+		},
+		{
+			title: "Análisis",
+			url: "/dashboard/analisis",
+			icon: IconReport,
+		},
+	],
+	navSecondary: [
+		{
+			title: "Configuración",
+			url: "/dashboard/configuracion",
+			icon: IconSettings,
+		},
+		{
+			title: "Ayuda",
+			url: "/dashboard/ayuda",
+			icon: IconHelp,
+		},
+	],
+	documents: [],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+	return (
+		<Sidebar collapsible="offcanvas" {...props}>
+			<SidebarHeader>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<SidebarMenuButton
+							asChild
+							className="data-[slot=sidebar-menu-button]:!p-1.5"
+						>
+							<a href="#">
+								<IconBlocks className="!size-5" />
+								<span className="text-base font-semibold">SmartBlocks</span>
+							</a>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
+			<SidebarContent>
+				<NavMain items={data.navMain} />
+				<NavSecondary items={data.navSecondary} className="mt-auto" />
+			</SidebarContent>
+			<SidebarFooter>
+				<NavUser user={data.user} />
+			</SidebarFooter>
+		</Sidebar>
+	);
+}
---
+++ components/nav-main.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { IconCirclePlusFilled, type Icon } from "@tabler/icons-react";
+
+import {
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarGroupLabel,
+} from "@/components/ui/sidebar";
+
+export function NavMain({
+	items,
+}: {
+	items: {
+		title: string;
+		url: string;
+		icon?: Icon;
+	}[];
+}) {
+	return (
+		<SidebarGroup>
+			<SidebarGroupContent className="flex flex-col gap-4">
+				<SidebarMenu>
+					<SidebarMenuItem className="flex items-center gap-2">
+						<SidebarMenuButton
+							tooltip="Agregar Producto"
+							className="bg-primary rounded-md text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
+						>
+							<IconCirclePlusFilled />
+							<span>Agregar producto</span>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+				<div className="h-px bg-border/60" /> {/* Divider */}
+				<SidebarMenu>
+					{items.map((item) => (
+						<SidebarMenuItem key={item.title}>
+							<SidebarMenuButton tooltip={item.title} asChild>
+								<a href={item.url}>
+									{item.icon && <item.icon />}
+									<span>{item.title}</span>
+								</a>
+							</SidebarMenuButton>
+						</SidebarMenuItem>
+					))}
+				</SidebarMenu>
+			</SidebarGroupContent>
+		</SidebarGroup>
+	);
+}
